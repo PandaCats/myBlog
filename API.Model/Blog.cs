c3e7d38162073d8690f3e0d8aa2c1a5ac8fa6105
PandaCats <39995144+PandaCats@users.noreply.github.com>
2019-05-25 10:14:30 +0800

    添加项目文件。

---
+++ API.Model/Blog.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+/// <summary>
+/// Blog的实体类
+/// </summary>
+public class Blog
+{
+    /// <summary>
+    /// id
+    /// </summary>
+    public int Id { get; set; }
+    /// <summary>
+    /// 姓名
+    /// </summary>
+    public string Name { get; set; }
+    /// <summary>
+    /// 年龄
+    /// </summary>
+    public int Age { get; set; }
+}
---
+++ API/Controllers/ValuesController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Linq;
+using API.Model;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+
+        /// <summary>
+        /// post
+        /// </summary>
+        /// <param name="blog">model实体类参数</param>
+        [HttpPost]
+        public void Post(Blog blog)
+        {
+        }
+    }
+}
