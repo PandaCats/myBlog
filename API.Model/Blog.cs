90d9d060256cb92c83aaac67de61b4d975d2f764
PandaCats <39995144+PandaCats@users.noreply.github.com>
2019-06-17 08:33:52 +0800

    .

--- API.Model/Blog.cs
+++ API.Model/Blog.cs
@@ -8,17 +8,11 @@ namespace Blog.Model
 {
     public class Panda
     {
-        /// <summary>
         /// id
-        /// </summary>
         public int Id { get; set; }
-        /// <summary>
         /// 姓名
-        /// </summary>
         public string Name { get; set; }
-        /// <summary>
         /// 年龄
-        /// </summary>
         public int Age { get; set; }
     }
 }
